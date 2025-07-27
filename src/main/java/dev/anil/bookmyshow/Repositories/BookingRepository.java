8a6ac462551b0c79dc4c124f241f33518b8cecac
Anil <pillalamarrianil@gmail.com>
2025-07-27 19:20:28 +0530

    Update BookingRepository.java

--- src/main/java/dev/anil/bookmyshow/Repositories/BookingRepository.java
+++ src/main/java/dev/anil/bookmyshow/Repositories/BookingRepository.java
@@ -3,6 +3,7 @@ package dev.anil.bookmyshow.Repositories;
 import dev.anil.bookmyshow.Models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
    Booking save(Booking booking);
